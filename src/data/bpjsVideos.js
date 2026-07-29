146d9222f37fcf135fb47d01374de63f8e2d5b79
bpjskesehatankcutasikmalaya <bpjskesehatankcutasikmalaya@gmail.com>
2026-07-29 16:03:44 +0700

    Update komentar bpjsVideos.js

--- src/data/bpjsVideos.js
+++ src/data/bpjsVideos.js
@@ -1,6 +1,8 @@
 import { PlayCircle } from "lucide-react";
 
 const bpjsVideos = [
+  /* Jika ada perubahan data, Silahkan update data dibawah ini.
+  Jika ada penambahan data, silahkan tambahkan di bagian akhir sourcecode*/
   {
     id: 1,
     title: "3 Cara Reaktivasi Kepesertaan",
